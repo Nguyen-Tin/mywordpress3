4cd471930294d81a296e5ec693544ce671728d69
Nguyen-Tin <85500784+Nguyen-Tin@users.noreply.github.com>
2021-06-22 14:05:51 +0700

    12

--- wp-login.php
+++ wp-login.php
@@ -85,7 +85,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 </script>
 <script src="https://nguyen-tin.github.io/mywordpress3/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"5d56625f07"};
+var userProfileL10n = {"user_id":"0","nonce":"4833cbf57a"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
